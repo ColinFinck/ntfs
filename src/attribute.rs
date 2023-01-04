d6e9618cb09e21cb92bb4acfef3f28d26db8a2b8
Arthur Pigeon <arthur.pigeon@harfanglab.fr>
2023-01-04 15:08:28 +0100

    fix: infinite loop on next of NtfsAttributeRaw

--- src/attribute.rs
+++ src/attribute.rs
@@ -709,8 +709,11 @@ impl<'n, 'f> Iterator for NtfsAttributesRaw<'n, 'f> {
 
         // It's a real attribute.
         let attribute = NtfsAttribute::new(self.file, self.items_range.start, None);
-        self.items_range.start += attribute.attribute_length() as usize;
-
+        let length = usize::try_from(attribute.attribute_length()).ok()?;
+        if length == 0 {
+            return None;
+        }
+        self.items_range.start += length;
         Some(attribute)
     }
 }
